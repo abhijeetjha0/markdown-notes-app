3f4cfc73326de5843b2fe95d8c33a4e351ef2c3d
abhijeetjha0 <abhijeetjha0@hotmail.com>
2026-08-23 15:19:41 +0530

    Fix firebase-admin transpiling issue ith Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    transpilePackages: ["firebase-admin"],
     sassOptions: {
         silenceDeprecations: [
             "legacy-js-api",
